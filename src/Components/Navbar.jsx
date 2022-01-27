210359121f5ed65f4a68325eaa29dd1ce9dbb969
SEBaharinasab <36398369+SEBaharinasab@users.noreply.github.com>
2022-01-27 13:21:02 +0330

    add style to home links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,23 +8,25 @@ function Navbar() {
    const dispatch = useDispatch();
    const themeDarkMode = useSelector(state => state.theme.darkMode);
    return (
-      <nav className={"bg-cyan-700 text-white flex items-center justify-center relative p-4"}>
-         <Link className={"text-3xl sm:text-4xl"} to={'/'}>Pelazio Shop!</Link>
-         <button
-            className={"aspect-square absolute -translate-y-1/2 top-1/2 right-4 p-2"}
-            onClick={() => dispatch(changeDarkMode())}
-         >
-            {themeDarkMode ? <FaMoon size={24} /> : <FaSun size={24} />}
-         </button>
-         <Link
-            className={"aspect-square absolute -translate-y-1/2 top-1/2 left-4"}
-            to={'/cart'}
-         >
-            <FaShoppingCart size={24} />
-            <span
-               className={" absolute -translate-y-1/2 translate-x-1/2 -top-0.5 -right-0.5 bg-red-500 text-white text-xs px-1 py-0.5  rounded-full"}
-            >100</span>
-         </Link>
+      <nav className={"bg-cyan-700 text-white"}>
+         <div className={"container mx-auto flex items-center justify-center relative p-4"}>
+            <Link className={"text-3xl sm:text-4xl"} to={'/'}>Pelazio Shop!</Link>
+            <button
+               className={"aspect-square absolute -translate-y-1/2 top-1/2 right-4 p-2"}
+               onClick={() => dispatch(changeDarkMode())}
+            >
+               {themeDarkMode ? <FaMoon size={24} /> : <FaSun size={24} />}
+            </button>
+            <Link
+               className={"aspect-square absolute -translate-y-1/2 top-1/2 left-4"}
+               to={'/cart'}
+            >
+               <FaShoppingCart size={24} />
+               <span
+                  className={" absolute -translate-y-1/2 translate-x-1/2 -top-0.5 -right-0.5 bg-red-500 text-white text-xs px-1 py-0.5  rounded-full"}
+               >100</span>
+            </Link>
+         </div>
       </nav>
    );
 }
--- src/Pages/Main/Home.page.jsx
+++ src/Pages/Main/Home.page.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 function Home() {
    return (
       <>
-         <Link to={"/products"}>Products</Link><br />
+         <h1 className={"text-3xl sm:text-5xl font-semibold"}>Home</h1>
+         <ul className={"inline-flex items-stretch gap-2 p-2 mt-4 border border-indigo-500 bg-indigo-100 rounded-lg"}>
+            <li><Link className={"w-full block py-2 px-3 border border-indigo-500 text-indigo-50 bg-indigo-500 hover:text-indigo-500 hover:border-indigo-500 hover:bg-indigo-50 rounded"} to={"/products"}>Products</Link></li>
+            <li><Link className={"w-full block py-2 px-3 border border-indigo-500 text-indigo-50 bg-indigo-500 hover:text-indigo-500 hover:border-indigo-500 hover:bg-indigo-50 rounded"} to={"/axios"}>Axios</Link></li>
+         </ul>
       </>
    );
 }
